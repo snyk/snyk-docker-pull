1cd893efc3a1b13318940c4d2cad88484f3c978c
Leon Goldberg <leon.otium@gmail.com>
2019-04-24 23:10:24 +0300

    fix: adding DockerPullResult to exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { DockerPull, RegistryConfig } from "./docker-pull";
+export { DockerPull, DockerPullResult, RegistryConfig } from "./docker-pull";
 export { LayersCacheClient } from  "./layers-cache-client";
