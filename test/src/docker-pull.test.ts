1d11ca9612cd04f684a3ddab4fd47b577469d0b3
Josh Michielsen <github@mickey.dev>
2020-06-04 13:06:17 +0100

    feat: allow optional username/password
    BREAKING CHANGE: DockerPull.pull method signature change

--- test/src/docker-pull.test.ts
+++ test/src/docker-pull.test.ts
@@ -1,4 +1,4 @@
-import { DockerPull } from "../../src/docker-pull";
+import { DockerPull, DockerPullOptions } from "../../src/docker-pull";
 import { removeImage } from "../utils";
 import * as path from "path";
 import * as fs from "fs";
@@ -6,19 +6,16 @@ import * as fs from "fs";
 jest.setTimeout(40000);
 
 test("private image pull and load", async () => {
-  const username = process.env.SNYK_DRA_DOCKER_HUB_USERNAME;
-  const password = process.env.SNYK_DRA_DOCKER_HUB_PASSWORD;
   const repo = process.env.SNYK_DRA_DOCKER_HUB_REPOSITORY;
+
+  const opt: DockerPullOptions = {
+    username: process.env.SNYK_DRA_DOCKER_HUB_USERNAME,
+    password: process.env.SNYK_DRA_DOCKER_HUB_PASSWORD
+  };
 
   const dockerPull: DockerPull = new DockerPull();
   const imageDigest: string = (
-    await dockerPull.pull(
-      username,
-      password,
-      "registry-1.docker.io",
-      repo,
-      "alpine"
-    )
+    await dockerPull.pull("registry-1.docker.io", repo, "alpine", opt)
   ).imageDigest;
   expect(imageDigest).toBeDefined();
 
@@ -26,21 +23,17 @@ test("private image pull and load", async () => {
 });
 
 test("private image pull and build", async () => {
-  const username = process.env.SNYK_DRA_DOCKER_HUB_USERNAME;
-  const password = process.env.SNYK_DRA_DOCKER_HUB_PASSWORD;
   const repo = process.env.SNYK_DRA_DOCKER_HUB_REPOSITORY;
+
+  const opt: DockerPullOptions = {
+    username: process.env.SNYK_DRA_DOCKER_HUB_USERNAME,
+    password: process.env.SNYK_DRA_DOCKER_HUB_PASSWORD,
+    loadImage: false
+  };
 
   const dockerPull: DockerPull = new DockerPull();
   const stagingDir = (
-    await dockerPull.pull(
-      username,
-      password,
-      "registry-1.docker.io",
-      repo,
-      "alpine",
-      {},
-      false
-    )
+    await dockerPull.pull("registry-1.docker.io", repo, "alpine", opt)
   ).stagingDir;
 
   expect(fs.existsSync(path.join(stagingDir.name, "image.tar"))).toBeTruthy();
