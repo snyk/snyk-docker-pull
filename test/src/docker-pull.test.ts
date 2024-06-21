d7d0abc8dde51912859a9436c5690cc3343d87a9
adrobuta <alexandra.drobut@snyk.io>
2024-06-21 11:35:47 +0300

    feat: support platform when retrieving image manifest UNIFY-163

--- test/src/docker-pull.test.ts
+++ test/src/docker-pull.test.ts
@@ -365,3 +365,52 @@ test("pull from public repo", async () => {
   fs.unlinkSync(tarPath);
   rmdirRecursive(opt.imageSavePath.split(path.sep));
 });
+
+test("pull from public repo arm64 image architecture", async () => {
+  const registry = "registry-1.docker.io";
+  const repo = "library/debian";
+  const tag = "12.0";
+  const os = "linux";
+  const arch = "arm64";
+  const debianArm64ManifestDigest =
+    "sha256:345b9265e89c123f994b724b9a4761dbc91c5759c144d01292ca56a338170801";
+  const arm64IndexDigest =
+    "sha256:3d868b5eb908155f3784317b3dda2941df87bbbbaa4608f84881de66d9bb297b";
+
+  const opt: DockerPullOptions = {
+    loadImage: false,
+    imageSavePath: "./custom/image/save/path",
+    reqOptions: {
+      acceptManifest: [
+        contentTypes.OCI_INDEX_V1,
+        contentTypes.OCI_MANIFEST_V1,
+        contentTypes.MANIFEST_V2,
+        contentTypes.MANIFEST_LIST_V2,
+      ].join(","),
+    },
+    platform: { os: os, architecture: arch },
+  };
+  // the custom path won't be create by the lib
+  fx.mkdirSync(opt.imageSavePath);
+
+  const dockerPull: DockerPull = new DockerPull();
+  const resp = await dockerPull.pull(registry, repo, tag, opt);
+
+  const manifestDigest = resp.manifestDigest;
+  const indexDigest = resp.indexDigest;
+
+  expect(manifestDigest).toBeDefined();
+  expect(indexDigest).toBeDefined();
+
+  expect(manifestDigest).toEqual(debianArm64ManifestDigest);
+  expect(indexDigest).toEqual(arm64IndexDigest);
+  const tarPath = path.join(resp.stagingDir.name, "image.tar");
+  expect(tarPath).toBe(path.join(resp.stagingDir.name, "image.tar"));
+  expect(fs.existsSync(tarPath)).toBeTruthy();
+
+  // it won't do nothing because we set imageSavePath
+  resp.stagingDir.removeCallback();
+  // clean up
+  fs.unlinkSync(tarPath);
+  rmdirRecursive(opt.imageSavePath.split(path.sep));
+});
